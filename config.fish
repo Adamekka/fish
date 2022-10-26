b7998277fa99a55412ab990cbe7057524f43a9ba
Adam Cvikl <adam.wasd4@gmail.com>
2022-10-26 17:43:22 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,3 +6,10 @@ end
 set -gx PNPM_HOME "/home/adamekka/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
+
+# Editor
+set -x EDITOR nvim
+set -x GIT_EDITOR $EDITOR
+
+# Prevent directories names from being shortened
+set fish_prompt_pwd_dir_length 0
