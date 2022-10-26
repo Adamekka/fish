0ddc5bd88ffbeb60a76996c980f1f3167d947fb8
Adam Cvikl <adam.wasd4@gmail.com>
2022-10-26 15:49:45 +0200

    shell now shows full pwd

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -73,7 +73,7 @@ function fish_prompt
         set arrow "$arrow_color# "
     end
 
-    set -l cwd $cyan(basename (prompt_pwd))
+    set -l cwd $cyan(prompt_pwd)
 
     set -l repo_info
     if set -l repo_type (_repo_type)
