3f3b6483c8bf292272b55bcb0a8082c8ad5c69f7
Adam Cvikl <adam.wasd4@gmail.com>
2022-10-26 17:34:33 +0200

    added missing comment

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# pnpm
 set -gx PNPM_HOME "/home/adamekka/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
