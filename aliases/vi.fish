07737a993da5f64d5e7ed05c6db541adf8cd0a0c
Adam Cvikl <adam.wasd4@gmail.com>
2022-11-02 18:04:34 +0100

    added checking what Vi-like editor do you use and setting aliases accordingly

--- aliases/vi.fish
+++ aliases/vi.fish
@@ -1,3 +1,14 @@
-alias v="nvim"
-alias vi="nvim"
-alias vim="nvim"
+# This file checks what Vi-like editor do you use and then sets aliases accordingly
+if test -e /usr/bin/nvim
+    alias v="nvim"
+    alias vi="nvim"
+    alias vim="nvim"
+else if test -e /usr/bin/vim
+    alias v="vim"
+    alias vi="vim"
+    alias nvim="vim"
+else if test -e /usr/bin/vi
+    alias v="vi"
+    alias vim="vi"
+    alias nvim="vi"
+end
