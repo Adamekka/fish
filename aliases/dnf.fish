13e651105de9826a27feeba7dde87a0f0c204e1d
Adam Cvikl <adam.wasd4@gmail.com>
2022-11-12 15:20:08 +0100

    added dnf aliases

---
+++ aliases/dnf.fish
@@ -0,0 +1,16 @@
+# Dnf stuff
+
+# install packages
+alias dnfi="sudo dnf install"
+
+# upgrade packages
+alias dnfu="sudo dnf upgrade"
+
+# remove packages
+alias dnfr="sudo dnf remove"
+
+# list packages
+alias dnfl="dnf list installed | rg"
+
+# search packages
+alias dnfs="dnf search"
