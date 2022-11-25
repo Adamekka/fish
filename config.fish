7cf8e45c937b812a907764093ae0f44e93324d5b
Adam Cvikl <adam.wasd4@gmail.com>
2022-11-25 15:10:46 +0100

    removed junk

--- config.fish
+++ config.fish
@@ -1,7 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # pnpm
 set -gx PNPM_HOME "/home/adamekka/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
