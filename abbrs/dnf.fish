8c7c6d56242bbf8129b460dc7c1eb080f8698fd0
Adam Cvikl <adam.wasd4@gmail.com>
2022-11-26 21:43:34 +0100

    dnf commands are now abbrs instead of aliases

---
+++ abbrs/dnf.fish
@@ -0,0 +1,16 @@
+# Dnf stuff
+
+# install packages
+abbr -a dnfi "sudo dnf install"
+
+# upgrade packages
+abbr -a dnfu "sudo dnf upgrade"
+
+# remove packages
+abbr -a dnfr "sudo dnf remove"
+
+# list packages
+abbr -a dnfl "dnf list installed | rg"
+
+# search packages
+abbr -a dnfs "dnf search"
