8abeeed17f11067e7158ec840e1189f042cb60f2
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-27 18:25:51 +0200

    fix updating Python packages

--- functions/update.fish
+++ functions/update.fish
@@ -72,7 +72,7 @@ function update
     # Python packages
     if type -q pip
         pip install --upgrade pip
-        pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
+        pip list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install -U
     end
 
     # Node.js packages
