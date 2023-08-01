522519181b3e82f1d4c1190189002b1136e0e6b5
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2023-08-02 01:05:09 +0200

    if pip has name pip3 use that to update python packages

--- functions/update.fish
+++ functions/update.fish
@@ -82,6 +82,9 @@ function update
     if type -q pip
         pip install --upgrade pip
         pip list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install -U
+    else if type -q pip3
+        pip3 install --upgrade pip
+        pip3 list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip3 install -U
     end
 
     # Node.js packages
