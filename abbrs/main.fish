6bc0f90a96100092940294753dabd3ee4b6a0893
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2023-11-27 18:44:25 +0100

    pyfmt

--- abbrs/main.fish
+++ abbrs/main.fish
@@ -12,5 +12,8 @@ abbr -a ka killall
 # swiftformat
 abbr -a sf "swiftformat **/*.swift"
 
+# autopep8
+abbr -a pyfmt "autopep8 -i -r **/*.py"
+
 # virt-manager
 abbr -a vm "virt-manager -c qemu:///session --no-fork"
