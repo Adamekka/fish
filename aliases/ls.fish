490b3ad323dcba1f2127b036f0492c8ed7df238a
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2024-11-23 00:58:14 +0100

    improve listing

--- aliases/ls.fish
+++ aliases/ls.fish
@@ -1,26 +1,14 @@
-################
-# Listing stuff#
-################
+if type -q eza
+    set ls_ eza
+else if type -q exa
+    set ls_ exa
+else
+    set -g ls_ ls
+end
 
-# Normal listing
-alias l="eza -F --icons --group-directories-first"
-alias ls="eza -F --icons --group-directories-first"
-
-# All listing (show stuff hidden from you)
-alias la="eza -aF --icons --group-directories-first"
-
-# Long listing
-alias ll="eza -lhF --icons --git --group-directories-first"
-# -h shows headers
-
-# Recursive listing
-alias lr="eza -RF --icons --group-directories-first"
-
-# Tree listing
-alias lt="eza -TF --icons --group-directories-first"
-
-# Directory listing
-alias ld="eza -DF --icons --group-directories-first"
-
-# -F displays type indicator by file names
-# folder/ executable*
+alias l="$ls_ -F --icons --group-directories-first"
+alias ls="$ls_ -F --icons --group-directories-first"
+alias la="$ls_ -aF --icons --group-directories-first"
+alias ll="$ls_ -lhF --icons --git --group-directories-first"
+alias lr="$ls_ -RF --icons --group-directories-first"
+alias lt="$ls_ -TF --icons --group-directories-first"
